a75e2a4bcb96d015491b01a2ca7fbd8aa752229d
mehradharmi <dharmendramehra15@gmail.com>
2026-05-24 21:03:03 +0530

    list and some work with it

---
+++ pratice/if_else.py
@@ -0,0 +1,8 @@
+cpu = 92
+
+if cpu > 90:
+    print("critical")
+elif cpu > 70:
+    print("Warning")
+else:
+    print("Normal CPU")
---
+++ pratice/list.py
@@ -0,0 +1,9 @@
+servers = ["web01", "web02", "db01"]
+
+print(servers[0])
+
+servers.append("db02")
+print(servers)
+
+servers.remove("web01")
+print(servers)
